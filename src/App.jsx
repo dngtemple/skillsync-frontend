f6b195b93eb67fe877ddc09db393b0657f91fd3d
Clinton Templeton <donnahaytempleton1230@gmail.com>
2025-10-19 17:34:32 +0000

    protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react'
 import './App.css'
 import Login from './login'
 import Signup from './signup'
@@ -8,20 +8,64 @@ import StepperFeild from './stepperFeild'
 import Notfound from './404'
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-         <Routes>
-          <Route path='/' element={<Dashboard/>}></Route>
-          <Route path="/stepper" element={<StepperFeild/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='*' element={<Notfound/>}></Route>
-         </Routes>
-      </BrowserRouter>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Protected routes (require user to be signed in) */}
+        <Route
+          path="/"
+          element={
+            <SignedIn>
+              <Dashboard />
+            </SignedIn>
+          }
+        />
+
+        <Route
+          path="/stepper"
+          element={
+            <SignedIn>
+              <StepperFeild />
+            </SignedIn>
+          }
+        />
+
+        {/* Public routes (accessible only when signed out) */}
+        <Route
+          path="/login"
+          element={
+            <SignedOut>
+              <Login />
+            </SignedOut>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <SignedOut>
+              <Signup />
+            </SignedOut>
+          }
+        />
+
+        {/* Redirect unauthenticated users trying to access protected routes */}
+        <Route
+          path="*"
+          element={
+            <>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+              <SignedIn>
+                <Notfound />
+              </SignedIn>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
